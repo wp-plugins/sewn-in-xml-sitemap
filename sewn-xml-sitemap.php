10ca8b6a94d3690f0824f22bdf98326f84910820
jcow <jcow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 05:12:09 +0000

    Version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/sewn-in-xml-sitemap/tags/2.0.0@1088695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sewn-xml-sitemap.php
+++ sewn-xml-sitemap.php
@@ -1,143 +1,174 @@
 <?php
-/*
-Plugin Name: Sewn In XML Sitemap
-Plugin URI: http://bitbucket.org/jupitercow/sewn-in-xml-sitemap
-Description: Simple system for building XML Sitemaps out of posts when saved.
-Version: 1.0.4
-Author: Jake Snyder
-Author URI: http://Jupitercow.com/
-License: GPLv2
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-------------------------------------------------------------------------
-Copyright 2014 Jupitercow, Inc.
+/**
+ * @link              https://github.com/jupitercow/sewn-in-xml-sitemap
+ * @since             1.0.0
+ * @package           Sewn_Xml_Sitemap
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sewn In XML Sitemap
+ * Plugin URI:        https://wordpress.org/plugins/sewn-in-xml-sitemap/
+ * Description:       Simple system for building XML Sitemaps out of posts when saved. Very simple and efficient.
+ * Version:           2.0.0
+ * Author:            Jupitercow
+ * Author URI:        http://Jupitercow.com/
+ * Contributor:       Jake Snyder
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       sewn-xml-sitemap
+ * Domain Path:       /languages
+ */
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+$class_name = 'Sewn_Xml_Sitemap';
+if (! class_exists($class_name) ) :
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
-
-if (! class_exists('sewn_xml_sitemap') ) :
-
-add_action( 'plugins_loaded', array('sewn_xml_sitemap', 'plugins_loaded') );
-
-add_action( 'init', array('sewn_xml_sitemap', 'init') );
-
-class sewn_xml_sitemap
+class Sewn_Xml_Sitemap
 {
 	/**
-	 * Class prefix
+	 * The unique prefix for Sewn In.
 	 *
-	 * @since 	1.0.0
-	 * @var 	string
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $prefix         The string used to uniquely prefix for Sewn In.
 	 */
-	const PREFIX = __CLASS__;
+	protected $prefix;
 
 	/**
-	 * Settings
+	 * The unique identifier of this plugin.
 	 *
-	 * @since 	1.0.0
-	 * @var 	string
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
 	 */
-	public static $settings = array(
-		'add_checkbox' => true,
-		'post_types'   => array('post','page'),
-	);
+	protected $plugin_name;
 
 	/**
-	 * Initialize the Class
+	 * The current version of the plugin.
 	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.0
-	 * @return	void
+	 * @since    2.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
 	 */
-	public static function init()
+	protected $version;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $settings       The array used for settings.
+	 */
+	protected $settings;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * @since    2.0.0
+	 */
+	public function __construct()
 	{
-		// actions
-		add_action( 'save_post',                  array(__CLASS__, 'create_sitemap') );
-		add_action( 'admin_init',                 array(__CLASS__, 'dependencies') );
-
-		// filters
-		add_filter( 'wp_list_pages_excludes',     array(__CLASS__, 'wp_list_pages_excludes') );
-
-		// Load ACF Fields
-		if ( self::$settings['add_checkbox'] ) self::register_field_groups();
+		$this->prefix      = 'sewn';
+		$this->plugin_name = strtolower(__CLASS__);
+		$this->version     = '2.0.0';
+		$this->settings    = array(
+			'add_checkbox' => true,
+			'post_types'   => array('post','page'),
+			'field_groups'      => array (
+				array(
+					'id'              => $this->plugin_name,
+					'title'           => __( 'XML Sitemap', $this->plugin_name ),
+					'fields'          => array (
+						array(
+							'label'        => __( 'Exclude from XML Sitemap', $this->plugin_name ),
+							'name'         => 'xml_sitemap_exclude',
+							'type'         => 'true_false',
+							'instructions' => __( 'This will keep the page from showing in the XML sitemap.', $this->plugin_name ),
+						),
+					),
+					'location'        => array (),
+					'menu_order'      => 0,
+					'context'         => 'normal',
+					'priority'        => 'low',
+					'label_placement' => 'top',
+				),
+			),
+		);
+		$this->settings = apply_filters( "{$this->prefix}/sitemap/settings", $this->settings );
 	}
 
 	/**
-	 * Check for dependencies
+	 * Load the plugin.
 	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.2
+	 * @since	2.0.0
 	 * @return	void
 	 */
-	public static function dependencies()
+	public function run()
 	{
-		if ( ! class_exists( 'acf' ) ) {
-			add_action( 'admin_notices', array(__CLASS__, 'acf_dependency_message') );
+		add_action( 'plugins_loaded', array($this, 'plugins_loaded') );
+		add_action( 'init',           array($this, 'init') );
+	}
+
+	/**
+	 * On plugins_loaded test if this can be combined with Sewn In Simple SEO.
+	 *
+	 * @since	1.0.0
+	 * @return	void
+	 */
+	public function plugins_loaded()
+	{
+		if ( class_exists('Sewn_Seo') ) {
+			$this->settings['add_checkbox'] = false;
+		} elseif ( ! class_exists('Sewn_Meta') ) {
+			require_once plugin_dir_path( __FILE__ ) . 'includes/sewn-meta/sewn-meta.php';
 		}
 	}
 
 	/**
-	 * Add a nag for ACF
+	 * Initialize the plugin once during run.
 	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.2
-	 * @return	void
-	 */
-	public static function acf_dependency_message()
-	{
-		?>
-		<div class="update-nag">
-			Sewn In XML Sitemap requires the <a href="http://wordpress.org/plugins/advanced-custom-fields/">Advanced Custom Fields</a> plugin to be installed and activated.
-		</div>
-		<?php
-	}
-
-	/**
-	 * Remove items from wp_list_pages() by default
-	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.2
-	 * @return	void
-	 */
-	public static function wp_list_pages_excludes( $exclude_array )
-	{
-		if ( apply_filters( self::PREFIX . '/wp_list_pages', true ) ) :
-			global $wpdb;
-			$sitemap_excludes = $wpdb->get_col( "SELECT ID FROM $wpdb->posts LEFT JOIN $wpdb->postmeta ON ($wpdb->posts.ID = $wpdb->postmeta.post_id) WHERE $wpdb->posts.post_type = 'page' AND ($wpdb->postmeta.meta_key='xml_sitemap_exclude' AND $wpdb->postmeta.meta_value!=0)" );
-
-			if ( $sitemap_excludes ) {
-				$exclude_array = array_merge( $exclude_array, $sitemap_excludes );
-			}
-		endif;
-
-		return $exclude_array;
-	}
-
-	/**
-	 * Create sitemap.xml file in the root directory
-	 *
-	 * @author  Tim Bowen, Jake Snyder
 	 * @since	1.0.0
 	 * @return	void
 	 */
-	public static function create_sitemap()
+	public function init()
+	{
+		add_action( 'save_post',                              array($this, 'create_sitemap') );
+		add_filter( 'wp_list_pages_excludes',                 array($this, 'wp_list_pages_excludes') );
+
+		add_filter( "{$this->prefix}/sitemap/exclude_field",  array($this, 'get_field') );
+
+		if ( $this->settings['add_checkbox'] ) {
+			$this->register_field_groups();
+		}
+	}
+
+	/**
+	 * Get post types.
+	 *
+	 * @since	2.0.0
+	 * @return	void
+	 */
+	public function post_types()
+	{
+		return apply_filters( "{$this->prefix}/sitemap/post_types", apply_filters( "{$this->prefix}/seo/post_types", apply_filters( 'sewn_seo/post_types', $this->settings['post_types'] ) ) );
+	}
+
+	/**
+	 * Create sitemap.xml file in the root directory.
+	 *
+	 * @since	1.0.0
+	 * @return	void
+	 */
+	public function create_sitemap()
 	{
 		$postsForSitemap = get_posts( array(
 			'numberposts' => -1,
 			'orderby'     => 'modified',
-			'post_type'   => apply_filters( self::PREFIX . '/post_types', apply_filters( 'sewn_seo/post_types', self::$settings['post_types'] ) ),
+			'post_type'   => $this->post_types(),
 			'order'       => 'DESC',
 			'meta_query'  => array(
 				'relation'    => 'OR',
@@ -154,30 +185,33 @@ class sewn_xml_sitemap
 			)
 		) );
 
-		$sitemap  = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-		$sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" count="'. count($postsForSitemap) .'">'."\n";
+		$sitemap  = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+		$sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" count="' . count($postsForSitemap) . '">' . "\n";
 
 		foreach ( $postsForSitemap as $post )
 		{
-			#$exclude = get_post_meta($post->ID, 'xml_sitemap_exclude', true);
-			#if ( $exclude ) continue;
 			setup_postdata($post);
-			$postdate = explode(" ", $post->post_modified);
-			$sitemap .= "\t".'<url>'."\n".
-				"\t\t".'<loc>' . get_permalink($post->ID) . '</loc>'."\n" .
-				"\t\t".'<lastmod>' . $postdate[0] . '</lastmod>'."\n" .
-				"\t\t".'<changefreq>monthly</changefreq>'."\n" .
-				"\t".'</url>'."\n";
+			$postdate  = explode( ' ', $post->post_modified );
+			$permalink = get_permalink( $post->ID );
+			$frequency = 'monthly';
+			$sitemap  .= "\t<url>\n" .
+				"\t\t<loc>{$permalink}</loc>\n" .
+				"\t\t<lastmod>{$postdate[0]}</lastmod>\n" .
+				"\t\t<changefreq>{$frequency}</changefreq>\n" .
+				"\t</url>\n";
 		}
 
 		$sitemap .= '</urlset>';
 
 		$filename = 'sitemap.xml';
-		if ( is_multisite() ) $filename = 'sitemap_' . sanitize_title(get_option('blogname')) . '.xml';
+		if ( is_multisite() ) {
+			list($name,$ext) = explode( '.', $filename );
+			$filename = "{$name}_" . sanitize_title(get_option('blogname')) . ".{$ext}";
+		}
 
-		$root = apply_filters( self::PREFIX . '/root', $_SERVER["DOCUMENT_ROOT"] );
+		$root = apply_filters( "{$this->prefix}/sitemap/root", $_SERVER["DOCUMENT_ROOT"] );
 
-		$fp = fopen( $root .'/'. $filename, 'w' );
+		$fp = fopen( "$root/$filename", 'w' );
 		if ( $fp )
 		{
 			fwrite($fp, $sitemap);
@@ -186,76 +220,53 @@ class sewn_xml_sitemap
 	}
 
 	/**
-	 * On plugins_loaded test if we can use frontend_notifications
+	 * Remove items from wp_list_pages() by default.
 	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.0
+	 * @since	1.0.2
 	 * @return	void
 	 */
-	public static function plugins_loaded()
+	public function wp_list_pages_excludes( $exclude_array )
 	{
-		if ( class_exists('sewn_seo') ) {
-			self::$settings['add_checkbox'] = false;
+		if ( apply_filters( "{$this->prefix}/sitemap/wp_list_pages", true ) )
+		{
+			global $wpdb;
+			$sitemap_excludes = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} LEFT JOIN {$wpdb->postmeta} ON ({$wpdb->posts}.ID = {$wpdb->postmeta}.post_id) WHERE {$wpdb->posts}.post_type = 'page' AND ({$wpdb->postmeta}.meta_key='xml_sitemap_exclude' AND {$wpdb->postmeta}.meta_value!=0)" );
+
+			if ( $sitemap_excludes ) {
+				$exclude_array = array_merge( $exclude_array, $sitemap_excludes );
+			}
+		}
+
+		return $exclude_array;
+	}
+
+	/**
+	 * Add the meta box.
+	 *
+	 * @since	2.0.0
+	 * @return	void
+	 */
+	public function register_field_groups()
+	{
+		foreach ( $this->settings['field_groups'] as $field_group ) {
+			do_action( "{$this->prefix}/meta/register_field_group", $field_group );
 		}
 	}
 
 	/**
-	 * Better SEO: ACF SEO Fields
+	 * Get the checkbox settings.
 	 *
-	 * @author  Jake Snyder
-	 * @since	1.0.0
+	 * @since	2.0.0
 	 * @return	void
 	 */
-	public static function register_field_groups()
+	public function get_field()
 	{
-		if (! function_exists("register_field_group") ) return;
-
-		$args = array (
-			'id' => 'acf_' . self::PREFIX,
-			'title' => 'XML Sitemap',
-			'fields' => array (
-				array (
-					'key' => 'field_52f9a1023c772',
-					'label' => 'Exclude from XML Sitemap',
-					'name' => 'xml_sitemap_exclude',
-					'type' => 'true_false',
-					'instructions' => 'This will keep the page from showing in the XML sitemap',
-					'message' => '',
-					'default_value' => 0,
-				),
-			),
-			'location' => array (),
-			'options' => array (
-				'position' => 'normal',
-				'layout' => 'default',
-				'hide_on_screen' => array (
-				),
-			),
-			'menu_order' => 0,
-		);
-
-		$default_location = array (
-			'param' => 'post_type',
-			'operator' => '==',
-			'value' => '',
-			'order_no' => 0,
-			'group_no' => 0,
-		);
-
-		$post_types = apply_filters( self::PREFIX . '/post_types', apply_filters( 'sewn_seo/post_types', self::$settings['post_types'] ) );
-
-		$i=0;
-		foreach ( $post_types as $post_type )
-		{
-			$new_location = $default_location;
-			$new_location['value'] = $post_type;
-			$new_location['group_no'] = $i;
-			$args['location'][] = array( $new_location );
-			$i++;
-		}
-
-		register_field_group( $args );
+		return $this->settings['field_groups'][0]['fields'][0];
 	}
 }
+
+$$class_name = new $class_name;
+$$class_name->run();
+unset($class_name);
 
 endif;
